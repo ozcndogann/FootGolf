78a623e08e2d85e163c7b019cde709325464587d
ozcndogann <ozcndogann02@gmail.com>
2023-06-20 14:12:08 +0300

    add photon, add demo scenes

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float Force;
+    public Transform target;
+    private bool isGrounded;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Update()
+    {
+        // Check if object is standing on the ground and speed is zero
+        if (isGrounded  && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 Shoot = (target.position - this.transform.position).normalized;
+        rb.AddForce(Shoot * Force, ForceMode.Impulse);
+    }
+}
