8a4154a3a18ba37309922becf7f8ca9fad1e463d
ozcndogann <ozcndogann02@gmail.com>
2023-07-11 12:10:47 +0300

    push

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -4,94 +4,94 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
-    private bool isIdle;
-    private bool isAiming;
-    [SerializeField] private float stopVelocity;
-    [SerializeField] private float shotPower;
-    private Rigidbody rb;
-    public Camera cam;
-    public bool isShooting;
+    [SerializeField] private LineRenderer lineRenderer; // aim için line
+    private bool isIdle; // top duruyor mu hareketli mi boolu
+    private bool isAiming; // oyuncu aim halinde mi boolu
+    [SerializeField] private float stopVelocity; // topun durmasý için min hýz
+    [SerializeField] private float shotPower; 
+    private Rigidbody rb; 
+    public Camera cam; 
+    public bool isShooting; // shoot hali boolu
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        isAiming = false;
-        lineRenderer.enabled = false;//baþta line'ýn görünmemesi için
+        rb = GetComponent<Rigidbody>(); 
+        isAiming = false; 
+        lineRenderer.enabled = false; // baþta line görünmemesi için
     }
+
     private void Update()
     {
-        if (rb.velocity.magnitude < stopVelocity)
+        if (rb.velocity.magnitude < stopVelocity) // topun durmasý için hýz kontrolü
         {
-           Stop();
-           ProcessAim();
+            Stop(); 
+            ProcessAim();
         }
-        
     }
+
     private void OnMouseDown()
     {
-        if (isIdle)
+        if (isIdle) 
         {
-            isAiming = true;
+            isAiming = true; 
         }
     }
-    
+
     private void ProcessAim()
     {
         if (!isAiming || !isIdle)
         {
-            return;
+            return; // exit method
         }
 
-        Vector3? worldPoint = CastMouseClickRay();
-        if (!worldPoint.HasValue)
+        Vector3? worldPoint = CastMouseClickRay(); // world pointi belirlemek için clickten ray yolla 
+        if (!worldPoint.HasValue) // ray bi þeye çarptý mý diye check
         {
-            return;
+            return; // exit method
         }
-        DrawLine(transform.position - (worldPoint.Value - transform.position)); 
-        if (Input.GetMouseButtonUp(0))
+        DrawLine(transform.position - (worldPoint.Value - transform.position)); // aim line çiz
+        if (Input.GetMouseButtonUp(0)) // parmaðýmý çektim mi
         {
-            Shoot(worldPoint.Value);
+            Shoot(worldPoint.Value); // shoot
         }
     }
-
 
     private void Shoot(Vector3 worldPoint)
     {
-        isAiming = false;
-        lineRenderer.enabled = false;
-        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+        isAiming = false; 
+        lineRenderer.enabled = false; // shoot çalýþýnca line görünmez olmalý
+        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z); // topun yerden gitmesi için y axisi ignorela *********
 
-        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
-        float lineLength = Vector3.Distance(transform.position, horizontalWorldPoint);
-        float force = Mathf.Min(lineLength, 1f) * shotPower; 
+        Vector3 direction = (horizontalWorldPoint - transform.position).normalized; //  toptan world pointe yönü hesapla
+        float lineLength = Vector3.Distance(transform.position, horizontalWorldPoint); // toptan world pointe mesafeyi hesapla
+        float force = Mathf.Min(lineLength, 1f) * shotPower; // topun gidiþ gücü line lengthe göre holacak
 
-        rb.AddForce(-direction * force);
-        isIdle = false;
+        rb.AddForce(-direction * force); // çektiðim yönün tersine gitmesi için -direction
+        isIdle = false; 
     }
-
 
     private void DrawLine(Vector3 worldPoint)
     {
-        Vector3 direction = worldPoint - transform.position;
-        float lineLength = direction.magnitude;
-        float maxLength = 1f; 
+        Vector3 direction = worldPoint - transform.position; // lineýn directioný
+        float lineLength = direction.magnitude; // lineýn uzunluðunun hesaplanmasý
+        float maxLength = 1f; // max line length
 
-        if (lineLength > maxLength)
+        if (lineLength > maxLength) // maxla current length kýyasý
         {
-            direction = direction.normalized * maxLength;
-            worldPoint = transform.position + direction;
+            direction = direction.normalized * maxLength; 
+            worldPoint = transform.position + direction; 
         }
 
-        Vector3[] positions = { transform.position, worldPoint };
-        lineRenderer.SetPositions(positions);
+        Vector3[] positions = { transform.position, worldPoint }; 
+        lineRenderer.SetPositions(positions); 
 
-        for (int i = 0; i < positions.Length; i++)
+        for (int i = 0; i < positions.Length; i++) // yukarýda aldýðýmýz positionlarý looplama
         {
-            positions[i].y = 0.28f;
+            positions[i].y = 0.28f; // lineýn y axisi fixleme
         }
 
-        lineRenderer.SetPositions(positions);
-        lineRenderer.enabled = true;
+        lineRenderer.SetPositions(positions); // update positions
+        lineRenderer.enabled = true; // line visible
     }
 
     private Vector3? CastMouseClickRay()
@@ -106,26 +106,23 @@ public class Ball : MonoBehaviour
             Input.mousePosition.y,
             Camera.main.nearClipPlane
             );
-        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
-        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar); 
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear); 
         RaycastHit hit;
-        if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity))
+        if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)) // neardan far'a ray yolla
         {
-            return hit.point;
+            return hit.point; // eðer ray bi þeye çarparsa return hit point
         }
         else
         {
-            return null;
+            return null; // eðer ray bi þeye çarpmazsa return null
         }
     }
+
     private void Stop()
     {
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-        isIdle = true;
+        rb.velocity = Vector3.zero; // topun velocitysini 0a eþitle
+        rb.angularVelocity = Vector3.zero; // topun angular velocitysini 0a eþitle
+        isIdle = true; 
     }
-    
-
-    
 }
-
