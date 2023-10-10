01afc81f85f398df08af65eb722715937533c813
ozcndogann <ozcndogann02@gmail.com>
2023-10-10 10:58:38 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -209,11 +209,11 @@ public class Ball : MonoBehaviour
             }
 
         }
-        if (!view.IsMine && !(bool)PhotonNetwork.LocalPlayer.CustomProperties["turn"])
-        {
-            // Spectator mode: Do not process input for spectator
-            return;
-        }
+        //if (!view.IsMine && !(bool)PhotonNetwork.LocalPlayer.CustomProperties["turn"])
+        //{
+        //    // Spectator mode: Do not process input for spectator
+        //    return;
+        //}
 
 
         if (gravityChanger)
