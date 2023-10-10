09007d8d3a721378d0ac26fd7a678af71b9ab9d3
ozcndogann <ozcndogann02@gmail.com>
2023-10-10 13:09:41 +0300

    h

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -310,7 +310,6 @@ public class Ball : MonoBehaviour
             PhotonNetwork.LocalPlayer.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
         }
         gravityChanger = false;
-        camlock = false;
 
     }
     
