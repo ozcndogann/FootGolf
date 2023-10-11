a34505db6db23e068a105dc0a9ea679bfc3a0a07
CnLfXX <canunsal91@hotmail.com>
2023-10-11 13:39:09 +0300

    buyur özcan brom

--- Assets/Scripts/Networking/ListingPlayer.cs
+++ Assets/Scripts/Networking/ListingPlayer.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using UnityEngine.UI;
+using TMPro;
 
 public class ListingPlayer : MonoBehaviour
 {
     [SerializeField] private PlayFabManager _text;
+    public TMP_Text NickName;
 
     public Player Player { get; private set; }
-
+    public void Start()
+    {
+        NickName.text = PlayerPrefs.GetString("Username");
+    }
     public void SetPlayerInfo(Player player)
     {
 
-
+       
         Player = player;
 
           player.NickName =  _text.name;
+        
     }
 
 
