772d68b5bd55f3481a03cbe0de3c8b6a2b2cb05d
ozcndogann <ozcndogann02@gmail.com>
2023-10-16 12:27:21 +0300

    a

--- Assets/Scripts/Player/ShotCounter.cs
+++ Assets/Scripts/Player/ShotCounter.cs
@@ -9,15 +9,23 @@ public class ShotCounter : MonoBehaviourPun
     {
         get
         {
-            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey(SCORE_KEY))
+            if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("Score"))
             {
-                return (int)PhotonNetwork.LocalPlayer.CustomProperties[SCORE_KEY];
+                int currentScore = (int)PhotonNetwork.LocalPlayer.CustomProperties["Score"];
+                currentScore++;
+                PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "Score", currentScore } });
+                Debug.Log("Directly Updated Score: " + currentScore);
+            }
+            else
+            {
+                PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "Score", 1 } });
+                Debug.Log("Directly Set Initial Score to 1");
             }
             return 0;
         }
         set
         {
-            PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { SCORE_KEY, ShotCount } });
+            PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { SCORE_KEY, val } });
         }
     }
 }
