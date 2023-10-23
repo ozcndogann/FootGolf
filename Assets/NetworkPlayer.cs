32d954a22af70dd137971d9224c0e670d197c48e
ozcndogann <96344964+ozcndogann@users.noreply.github.com>
2023-10-23 11:41:45 +0300

    Update NetworkPlayer.cs

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -2,77 +2,66 @@ using Photon.Pun;
 using UnityEngine;
 
 public class NetworkPlayer : MonoBehaviourPun, IPunObservable
+{
+
+
+    Vector3 latestPos;
+    Quaternion latestRot;
+    //Lag compensation
+    float currentTime = 0;
+    double currentPacketTime = 0;
+    double lastPacketTime = 0;
+    Vector3 positionAtLastPacket = Vector3.zero;
+    Quaternion rotationAtLastPacket = Quaternion.identity;
+    public bool teleportIfFar;
+    public float teleportIfFarDistance;
+    [Header("Lerping[Experimental]")]
+    public float smoothpos = 0.5f;
+    public float smoothRot = 0.5f;
+    void Awake()
     {
+        PhotonNetwork.SendRate = 30;
+        PhotonNetwork.SerializationRate = 10;
 
-        protected Ball ball;
-        protected Vector3 RemotePlayerPosition;
-        protected float RemoteLookX;
-        protected float RemoteLookZ;
-        protected float LookXVel;
-        protected float LookZVel;
+    }
 
-        private void Awake()
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
         {
-            ball = GetComponent<Ball>();
-
-            //destroy the controller if the player is not controlled by me
-            if (!photonView.IsMine && GetComponent<Ball>() != null)
-                Destroy(GetComponent<Ball>());
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
         }
-
-        public void Update()
+        else
         {
-            if (photonView.IsMine)
-                return;
+            latestPos = (Vector3)stream.ReceiveNext();
+            latestRot = (Quaternion)stream.ReceiveNext();
 
-            var LagDistance = RemotePlayerPosition - transform.position;
+            //Lag compensation
+            currentTime = 0.0f;
+            lastPacketTime = currentPacketTime;
+            currentPacketTime = info.SentServerTime;
+            positionAtLastPacket = transform.position;
+            rotationAtLastPacket = transform.rotation;
 
-            //High distance => sync is to much off => send to position
-            if (LagDistance.magnitude > 5f)
-            {
-                transform.position = RemotePlayerPosition;
-                LagDistance = Vector3.zero;
-            }
-
-            //ignore the y distance
-            LagDistance.y = 0;
-
-            //if (LagDistance.magnitude < 0.11f)
-            //{
-            //    //Player is nearly at the point
-            //    Player.Input.RunX = 0;
-            //    Player.Input.RunZ = 0;
-            //}
-            //else
-            //{
-            //    //Player has to go to the point
-            //    Player.Input.RunX = LagDistance.normalized.x;
-            //    Player.Input.RunZ = LagDistance.normalized.z;
-            //}
-
-            ////jump if the remote player is higher than the player on the current client
-            //Player.Input.Jump = RemotePlayerPosition.y - transform.position.y > 0.2f;
-
-            ////Look Smooth
-            //Player.Input.LookX = Mathf.SmoothDamp(Player.Input.LookX, RemoteLookX, ref LookXVel, 0.2f);
-            //Player.Input.LookZ = Mathf.SmoothDamp(Player.Input.LookZ, RemoteLookZ, ref LookZVel, 0.2f);
-
-        }
-
-        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-        {
-            if (stream.IsWriting)
-            {
-                stream.SendNext(transform.position);
-                //stream.SendNext(Player.Input.LookX);
-                //stream.SendNext(Player.Input.LookZ);
-            }
-            else
-            {
-                RemotePlayerPosition = (Vector3)stream.ReceiveNext();
-                RemoteLookX = (float)stream.ReceiveNext();
-                RemoteLookZ = (float)stream.ReceiveNext();
-
-            }
         }
     }
+    void FixedUpdate()
+    {
+        if (photonView.IsMine) return;
+        double timeToReachGoal = currentPacketTime - lastPacketTime;
+        currentTime += Time.deltaTime;
+
+        //Update remote player
+        transform.position = Vector3.Lerp(positionAtLastPacket, latestPos,
+(float)(currentTime / timeToReachGoal));
+        transform.rotation = Quaternion.Lerp(rotationAtLastPacket, latestRot,
+(float)(currentTime / timeToReachGoal));
+
+        if (Vector3.Distance(transform.position, latestPos) > teleportIfFarDistance)
+        {
+            transform.position = latestPos;
+        }
+    }
+}
