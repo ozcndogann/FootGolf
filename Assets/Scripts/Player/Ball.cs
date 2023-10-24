e637a8dd6ac40767efbea77c33913d3614721084
ozcndogann <96344964+ozcndogann@users.noreply.github.com>
2023-10-24 21:57:45 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -285,7 +285,7 @@ public class Ball : MonoBehaviour
                         else
                         {
                             AnimationFootballer.lineRendererController = false;
-                            shooted = true;
+                            shooted = false;
                             shootCloser = true;
                             Zoom.changeFovBool = false;
                             if (PhotonNetwork.CurrentRoom.PlayerCount != 1)
