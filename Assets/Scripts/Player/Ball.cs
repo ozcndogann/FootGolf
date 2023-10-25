3d000688e52e88a519e9b58ce78bfc87cf4e11f9
ozcndogann <96344964+ozcndogann@users.noreply.github.com>
2023-10-25 12:06:56 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -749,33 +749,30 @@ public class Ball : MonoBehaviour
             Debug.Log("returnoncesi");
             return null;
         }
+        Debug.Log("returnsonrasý");
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.farClipPlane
+            );
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane
+            );
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+
+        if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)) // neardan far'a ray yolla
+        {
+            return hit.point; // eðer ray bi þeye çarparsa return hit point
+        }
         else
         {
-            Debug.Log("returnsonrasý");
-            Vector3 screenMousePosFar = new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                Camera.main.farClipPlane
-                );
-            Vector3 screenMousePosNear = new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                Camera.main.nearClipPlane
-                );
-            Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
-            Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
-            RaycastHit hit;
-
-            if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)) // neardan far'a ray yolla
-            {
-                return hit.point; // eðer ray bi þeye çarparsa return hit point
-            }
-            else
-            {
-                return null; // eðer ray bi þeye çarpmazsa return null
-            }
+            return null; // eðer ray bi þeye çarpmazsa return null
         }
-        
+
 
     }
 
