620f4ddb99516a1c07e1caae0ade18d279b33e28
ozcndogann <96344964+ozcndogann@users.noreply.github.com>
2023-10-25 16:35:34 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -51,6 +51,7 @@ public class Ball : MonoBehaviour
     public LayerMask ground;
     Ray rayNorm;
     Ray rayTri;
+    bool götveren;
     #endregion
 
 
@@ -322,11 +323,6 @@ public class Ball : MonoBehaviour
                             timer = 20f;
                         }
                     }
-                    else
-                    {
-                        Debug.Log("BOYLE OYUNU SIKEYIM CEREBRUM HAYATIMI GERI VER");
-                        worldPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-                    }
                 }
 
             }
@@ -564,6 +560,7 @@ public class Ball : MonoBehaviour
         {
             if ((bool)PhotonNetwork.LocalPlayer.CustomProperties["turn"])
             {
+                
                 Debug.Log("mousedown");
             }
         }
@@ -658,7 +655,12 @@ public class Ball : MonoBehaviour
     
     private void ProcessAim()
     {
-        
+        if (!götveren)
+        {
+            Vector3 position0 = lineRenderer.GetPosition(0);
+            lineRenderer.SetPosition(1, position0);
+            götveren = true;
+        }
         if (!isAiming || !isIdle)
         {
             gravityChanger = false;
@@ -687,6 +689,7 @@ public class Ball : MonoBehaviour
         {
             AnimationFootballer.lineRendererOn = false;
             shooted = true;
+            götveren = false;
             Zoom.changeFovBool = true;
         }
 
@@ -794,23 +797,24 @@ public class Ball : MonoBehaviour
         //    Debug.Log("returnoncesi");
         //    return null;
         //}
-        
-        if (Input.GetMouseButton(0))
-        {
-            screenMousePosFar = new Vector3(
+
+        //if (Input.GetMouseButton(0))
+        //{
+
+        //}
+        screenMousePosFar = new Vector3(
             Input.mousePosition.x,
             Input.mousePosition.y,
             Camera.main.farClipPlane
             );
-            screenMousePosNear = new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                Camera.main.nearClipPlane
-                );
-            worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
-            worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
-        }
-        
+        screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane
+            );
+        worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+
         RaycastHit hit;
         if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)) // neardan far'a ray yolla
         {
