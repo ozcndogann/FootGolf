063685fa208389f94ba436f2ee49f9e453807c2e
alperentnc <alprn_08_@hotmail.com>
2023-10-29 07:47:10 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -338,7 +338,7 @@ public class Ball : MonoBehaviour
                     }
                     else
                     {
-                        CastMouseClickRay();
+                        worldPoint=CastMouseClickRay();
                     }
                 }
 
