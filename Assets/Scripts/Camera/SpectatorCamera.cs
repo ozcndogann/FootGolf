e9005f7b6d5aaeaf852ebd9cafac09a44d5fe9b3
ozcndogann <ozcndogann02@gmail.com>
2023-12-02 13:42:39 +0300

    a

---
+++ Assets/Scripts/Camera/SpectatorCamera.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorCamera : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        CheckTurn();
+    }
+    private void CheckTurn()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.CustomProperties.ContainsKey("IsTurn") && (bool)player.CustomProperties["IsTurn"])
+            {
+                Debug.Log("It's the turn of player: " + player.NickName);
+            }
+        }
+    }
+}
