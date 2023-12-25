078945ca779f8b5b4f43461a6054f4fc4e5ad0d5
Can Gezer <gezer17.cg@gmail.com>
2023-12-25 16:46:46 +0300

    bunker

--- Assets/Terrain/RainyDynamic.cs
+++ Assets/Terrain/RainyDynamic.cs
@@ -13,21 +13,16 @@ public class RainyDynamic : MonoBehaviour
     {
         rb = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody>();
         rbphy = GameObject.FindGameObjectWithTag("Ball").GetComponent<SphereCollider>().material;
-
-        rb.mass = 1;
-        rb.angularDrag = 0.05f;
-        rb.drag = 0.075f;
-        rbphy.bounciness = 0.5f;
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Ball")
         {  
-            rb.mass = 1.1f;
+            rb.mass = 1.05f;
             rb.angularDrag = 0.05f;
             rb.drag = 0.075f;
-            rbphy.bounciness = 0.4f;
+            rbphy.bounciness = 0.5f;
         }
     }
 }
