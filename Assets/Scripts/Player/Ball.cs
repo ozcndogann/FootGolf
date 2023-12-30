90de668db06466460234db7f89f63d557e2c5455
ozcndogann <96344964+ozcndogann@users.noreply.github.com>
2023-12-31 00:53:12 +0300

    Update Ball.cs

--- Assets/Scripts/Player/Ball.cs
+++ Assets/Scripts/Player/Ball.cs
@@ -701,51 +701,20 @@ public class Ball : MonoBehaviour
                         if ((bool)player.CustomProperties["turn"])
                         {
                             player.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", false } });
-                            Debug.Log("0");
-                            if ((bool)player.CustomProperties["holeC"])
-                            {
-                                Debug.Log("01");
-                                player.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                            }
-                            else
-                            {
-                                if ((bool)player.GetNext().CustomProperties["holeC"])
-                                {
-                                    Debug.Log("01");
-                                    player.GetNext().GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                                }
-                                else
-                                {
-                                    Debug.Log("04");
-                                    player.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                                }
-                            }
-                            
-                            //else if ((bool)player.GetNext().GetNext().CustomProperties["holeC"])
-                            //{
-                            //    Debug.Log("02");
-                            //    player.GetNext().GetNext().GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                            //}
-                            //else if ((bool)player.GetNext().GetNext().GetNext().CustomProperties["holeC"])
-                            //{
-                            //    Debug.Log("03");
-                            //    player.GetNext().GetNext().GetNext().GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                            //}
-                            //else
-                            //{
-                            //    Debug.Log("04");
-                            //    player.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                            //}
+                            player.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
                         }
-                    } 
-                    
+                    }
+                    if (!(bool)player.GetNext().CustomProperties["holeC"])
+                    {
+                        shotClicked = false;
+                        nextPlayerTurn = false;
+                    }
                 }
-                //PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", false } });
-                //PhotonNetwork.LocalPlayer.GetNext().SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "turn", true } });
-                shotClicked = false;
-                nextPlayerTurn = false;
+                
+                
             }
         }
+        
     }
     IEnumerator ShootedBool()
     {
