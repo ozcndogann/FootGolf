8d4ca9fe5c2f59a77c86fe168a7f32ad5bcc1c70
ozcndogann <ozcndogann02@gmail.com>
2024-02-22 11:38:55 +0300

    Update ChangeCameras.cs

--- Assets/Scripts/Camera/ChangeCameras.cs
+++ Assets/Scripts/Camera/ChangeCameras.cs
@@ -52,86 +52,88 @@ public class ChangeCameras : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(MainCam.transform.rotation.eulerAngles.y);
-        //cam1.transform.position = new Vector3(transform.position.x - 4, transform.position.y + 1.4f, transform.position.z);// -90 main cameradan
-        cam1.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 90, 0);
-        //cam2.transform.position = new Vector3(transform.position.x + 4, transform.position.y + 1.4f, transform.position.z);// -270 main cameradan
-        cam2.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 270, 0);
-        //cam3.transform.position = new Vector3(transform.position.x, transform.position.y + 1.4f, transform.position.z - 4);// -180 main cameradan
-        cam3.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 180, 0);
-        //cam4.transform.position = new Vector3(transform.position.x, transform.position.y + 1.4f, transform.position.z + 4);// main camerayla eþit
-        cam4.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y, 0);
-        //cam5.transform.position = new Vector3(transform.position.x + 4, transform.position.y + 1.4f, transform.position.z - 4); // -225 main cameradan
-        cam5.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 225, 0);
-        if (AnimationFootballer.lineRendererOn == false && Input.GetMouseButton(0) && rb.velocity.magnitude < 0.75f && Ball.shooted==false)
+        if (view.IsMine)
         {
-            cam1.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
-            cam2.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
-            cam3.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
-            cam4.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
-            cam5.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
-        }
-        else if(AnimationFootballer.lineRendererOn == true && Input.GetMouseButton(0) && rb.velocity.magnitude < 0.75f && Ball.shooted==false)
-        {
-            cam1.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
-            cam2.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
-            cam3.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
-            cam4.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
-            cam5.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
-        }
-        if (AnimationFootballer.waitForShoot || AnimationFootballer.waitForShootTri)
-        {
-            disCam1 = target.transform.position - cam1.transform.position;
-            disCam2 = target.transform.position - cam2.transform.position;
-            disCam3 = target.transform.position - cam3.transform.position;
-            disCam4 = target.transform.position - cam4.transform.position;
-            disCam5 = target.transform.position - cam5.transform.position;
-            if (!randomerBool)
+            Debug.Log(MainCam.transform.rotation.eulerAngles.y);
+            //cam1.transform.position = new Vector3(transform.position.x - 4, transform.position.y + 1.4f, transform.position.z);// -90 main cameradan
+            cam1.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 90, 0);
+            //cam2.transform.position = new Vector3(transform.position.x + 4, transform.position.y + 1.4f, transform.position.z);// -270 main cameradan
+            cam2.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 270, 0);
+            //cam3.transform.position = new Vector3(transform.position.x, transform.position.y + 1.4f, transform.position.z - 4);// -180 main cameradan
+            cam3.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 180, 0);
+            //cam4.transform.position = new Vector3(transform.position.x, transform.position.y + 1.4f, transform.position.z + 4);// main camerayla eþit
+            cam4.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y, 0);
+            //cam5.transform.position = new Vector3(transform.position.x + 4, transform.position.y + 1.4f, transform.position.z - 4); // -225 main cameradan
+            cam5.transform.rotation = Quaternion.Euler(0, MainCam.transform.rotation.eulerAngles.y - 225, 0);
+            if (AnimationFootballer.lineRendererOn == false && Input.GetMouseButton(0) && rb.velocity.magnitude < 0.75f && Ball.shooted == false)
             {
-                random = Random.Range(0, 5);
-                randomerBool = true;
+                cam1.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
+                cam2.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
+                cam3.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
+                cam4.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
+                cam5.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 60);
             }
+            else if (AnimationFootballer.lineRendererOn == true && Input.GetMouseButton(0) && rb.velocity.magnitude < 0.75f && Ball.shooted == false)
+            {
+                cam1.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
+                cam2.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
+                cam3.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
+                cam4.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
+                cam5.transform.RotateAround(target.transform.position, Vector3.up, MoveAroundObject.rotationaroundyaxis / 300);
+            }
+            if (AnimationFootballer.waitForShoot || AnimationFootballer.waitForShootTri)
+            {
+                disCam1 = target.transform.position - cam1.transform.position;
+                disCam2 = target.transform.position - cam2.transform.position;
+                disCam3 = target.transform.position - cam3.transform.position;
+                disCam4 = target.transform.position - cam4.transform.position;
+                disCam5 = target.transform.position - cam5.transform.position;
+                if (!randomerBool)
+                {
+                    random = Random.Range(0, 5);
+                    randomerBool = true;
+                }
                 switch (random)
                 {
                     case 0:
-                    //if (cam1W.IsMine)
-                    //{
-                    cam1.GetComponent<Camera>().enabled = true;
-                    MainCam.GetComponent<Camera>().enabled = false;
-                    //}  
-                    break;
+                        //if (cam1W.IsMine)
+                        //{
+                        cam1.GetComponent<Camera>().enabled = true;
+                        MainCam.GetComponent<Camera>().enabled = false;
+                        //}  
+                        break;
                     case 1:
-                    //if (cam2W.IsMine)
-                    //{
-                    cam2.GetComponent<Camera>().enabled = true;
-                    MainCam.GetComponent<Camera>().enabled = false;
-                    //}   
-                    break;
+                        //if (cam2W.IsMine)
+                        //{
+                        cam2.GetComponent<Camera>().enabled = true;
+                        MainCam.GetComponent<Camera>().enabled = false;
+                        //}   
+                        break;
                     case 2:
-                    //if (cam3W.IsMine)
-                    //{
-                    cam3.GetComponent<Camera>().enabled = true;
-                    MainCam.GetComponent<Camera>().enabled = false;
-                    //}
-                    break;
+                        //if (cam3W.IsMine)
+                        //{
+                        cam3.GetComponent<Camera>().enabled = true;
+                        MainCam.GetComponent<Camera>().enabled = false;
+                        //}
+                        break;
                     case 3:
-                    //if (cam4W.IsMine)
-                    //{
-                    cam4.GetComponent<Camera>().enabled = true;
-                    MainCam.GetComponent<Camera>().enabled = false;
-                    //}
-                    break;
+                        //if (cam4W.IsMine)
+                        //{
+                        cam4.GetComponent<Camera>().enabled = true;
+                        MainCam.GetComponent<Camera>().enabled = false;
+                        //}
+                        break;
                     case 4:
-                    //if (cam5W.IsMine)
-                    //{
-                    cam5.GetComponent<Camera>().enabled = true;
-                    MainCam.GetComponent<Camera>().enabled = false;
-                    //}
-                    break;
+                        //if (cam5W.IsMine)
+                        //{
+                        cam5.GetComponent<Camera>().enabled = true;
+                        MainCam.GetComponent<Camera>().enabled = false;
+                        //}
+                        break;
                 }
-        }
-        else
-        {
+            }
+            else
+            {
                 randomerBool = false;
                 MainCam.GetComponent<Camera>().enabled = true;
                 cam1.GetComponent<Camera>().enabled = false;
@@ -139,19 +141,22 @@ public class ChangeCameras : MonoBehaviour
                 cam3.GetComponent<Camera>().enabled = false;
                 cam4.GetComponent<Camera>().enabled = false;
                 cam5.GetComponent<Camera>().enabled = false;
-        }
-        if (rb.velocity.magnitude < 0.75f && cameraChanger == false)
-        {
-            cam1.transform.position = target.transform.position - disCam1;
-            cam2.transform.position = target.transform.position - disCam2;
-            cam3.transform.position = target.transform.position - disCam3;
-            cam4.transform.position = target.transform.position - disCam4;
-            cam5.transform.position = target.transform.position - disCam5;
-            cameraChanger = true;
-        }
-        else if (rb.velocity.magnitude > 0.75f)
-        {
-            cameraChanger = false;
+            }
+            if (rb.velocity.magnitude < 0.75f && cameraChanger == false)
+            {
+                cam1.transform.position = target.transform.position - disCam1;
+                cam2.transform.position = target.transform.position - disCam2;
+                cam3.transform.position = target.transform.position - disCam3;
+                cam4.transform.position = target.transform.position - disCam4;
+                cam5.transform.position = target.transform.position - disCam5;
+                cameraChanger = true;
+            }
+            else if (rb.velocity.magnitude > 0.75f)
+            {
+                cameraChanger = false;
+            }
         }
     }
+
+        
 }
